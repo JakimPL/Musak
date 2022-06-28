c930cc134cd5941e79487af2dfa0ee119c753e75
JakimPL <jakimpl@gmail.com>
2022-06-28 10:52:11 +0200

    Generalized exports

--- inversions/models.py
+++ inversions/models.py
@@ -1,9 +1,13 @@
-from chordinversions.exporter import Exporter
+import json
+import os
+
+from chordinversions.exporter import to_abjad
 from chordinversions.generator import get_random_chord_inversion, generate_all_inversions
 from chordinversions.inversion import ChordInversion
 
 from inversions.services import get_chords_definitions
 from shared.directory import create_directory
+from shared.exporter import Exporter
 
 
 class ChordInversionModel:
@@ -12,10 +16,6 @@ class ChordInversionModel:
         self._chords: dict[str, list[int]] = chords if chords else get_chords_definitions()
         self._inversions: dict[str, list[ChordInversion]] = generate_all_inversions(self._chords)
         self._settings: dict = settings
-        self._exporter: Exporter = Exporter(
-            sequential=settings['sequential'],
-            tempo=settings['tempo']
-        )
 
     def get_max_inversion_index(self) -> int:
         return max([len(chord) for chord in self._inversions.values()])
@@ -27,11 +27,22 @@ class ChordInversionModel:
             highest_note=self._settings['highest_note']
         )
 
+    @staticmethod
+    def export_info(chord_inversion: ChordInversion, chord_info_path: str):
+        with open(chord_info_path, 'w') as file:
+            data = chord_inversion._asdict()
+            data['base_note'] = chord_inversion.get_base_note_name()
+            json.dump(data, file)
+
     def export_chord_inversion(self, path: str, chord_inversion: ChordInversion = None):
         if chord_inversion is None:
             chord_inversion = self.get_random_chord_inversion()
 
-        self._exporter.export(chord_inversion, path)
+        score = to_abjad(chord_inversion, self._settings['tempo'], self._settings['sequential'])
+        exporter = Exporter('chord', ignore_score=True)
+
+        self.export_info(chord_inversion, os.path.join(path, 'chord.json'))
+        exporter.export(score, path)
 
     def generate(self) -> str:
         uuid64, directory = create_directory()
---
+++ shared/exporter.py
@@ -0,0 +1,104 @@
+import os
+import pathlib
+from typing import Union
+
+import abjad
+import pydub
+from rhygen.modules.converted import to_audio
+
+SOUNDFONT = 'st_concert.sf2'
+AUDIO_FORMAT = 'wav'
+CONVERT_TO_MP3 = True
+IGNORE_MIDI = False
+IGNORE_AUDIO = False
+IGNORE_SCORE = True
+
+
+class Exporter:
+    def __init__(
+            self,
+            name: str,
+            sf2: str = SOUNDFONT,
+            ignore_midi: bool = IGNORE_MIDI,
+            ignore_audio: bool = IGNORE_AUDIO,
+            ignore_score: bool = IGNORE_SCORE,
+            audio_format: str = AUDIO_FORMAT,
+            convert_to_mp3: bool = CONVERT_TO_MP3
+    ):
+        self.name: str = name
+
+        self.soundfont_path: str = os.path.join(os.getcwd(), 'soundfont', sf2)
+        self.audio_format: str = audio_format
+        self.convert_to_mp3: bool = convert_to_mp3
+
+        self.ignore_midi: bool = ignore_midi
+        self.ignore_audio: bool = ignore_audio
+        self.ignore_score: bool = ignore_score
+
+    @staticmethod
+    def prepare_ly_file(score: abjad.Score) -> abjad.LilyPondFile:
+        score_block = abjad.Block('score', items=[score])
+        midi_block = abjad.Block('midi')
+        score_block.items.append(midi_block)
+        return abjad.LilyPondFile([score_block])
+
+    def export_score(self, score: Union[abjad.Score, abjad.LilyPondFile], directory: str = None, **kwargs) -> str:
+        if directory is None:
+            directory = os.getcwd()
+
+        original_path = os.path.join(directory, '{name}_uncropped.png'.format(name=self.name))
+        path = os.path.join(directory, '{name}.png'.format(name=self.name))
+
+        try:
+            abjad.persist.as_png(score, original_path, remove_ly=True, resolution=250, flags='--png -dcrop', **kwargs)
+        except AttributeError:
+            pass
+
+        cropped_path = original_path[:-4] + '.cropped.png'
+        os.rename(cropped_path, path)
+
+        return path
+
+    def export_midi(self, score: Union[abjad.Score, abjad.LilyPondFile], directory: str = None, **kwargs) -> str:
+        if directory is None:
+            directory = os.getcwd()
+
+        original_path = os.path.join(directory, '{name}.midi'.format(name=self.name))
+        path = str(pathlib.Path(original_path).with_suffix('.mid'))
+
+        ly_file = Exporter.prepare_ly_file(score) if isinstance(score, abjad.Score) else score
+        abjad.persist.as_midi(ly_file, original_path, remove_ly=False, **kwargs)
+
+        os.rename(original_path, path)
+
+        return path
+
+    def export_audio(self, midi_path: str, audio_path: str) -> str:
+        to_audio(self.soundfont_path, midi_path, audio_path, out_type=self.audio_format)
+        if self.convert_to_mp3:
+            mp3_path = str(pathlib.Path(audio_path).with_suffix('.mp3'))
+            sound = pydub.AudioSegment.from_wav(audio_path)
+            sound.export(mp3_path, format='mp3')
+            return mp3_path
+        else:
+            return audio_path
+
+    def export(self, score: abjad.Score, directory: str) -> tuple[str, str, str]:
+        if not isinstance(score, abjad.Score):
+            raise TypeError('expected abjad.Score, got {type}'.format(type=type(score)))
+
+        image_path = ''
+        midi_path = ''
+        mp3_path = ''
+
+        if not self.ignore_score:
+            image_path = self.export_score(score, directory)
+
+        if not self.ignore_midi:
+            midi_path = self.export_midi(score, directory)
+
+            if not self.ignore_audio:
+                audio_path = os.path.join(directory, '{name}.wav'.format(name=self.name))
+                mp3_path = self.export_audio(midi_path, audio_path)
+
+        return image_path, midi_path, mp3_path
